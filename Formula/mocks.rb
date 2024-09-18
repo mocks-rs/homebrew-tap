20cee040174dbb40e0d3642b7db68f6aabf6254c
codemountains <4kz12zz@gmail.com>
2024-09-18 23:29:28 +0900

    :bookmark: 0.3.1

--- Formula/mocks.rb
+++ Formula/mocks.rb
@@ -1,9 +1,9 @@
 class Mocks < Formula
     desc "Get a mock REST APIs with zero coding within seconds."
     homepage "https://github.com/mocks-rs/mocks"
-    url "https://github.com/mocks-rs/mocks/releases/download/0.3.0/mocks-0.3.0-x86_64-apple-darwin.tar.gz"
-    sha256 "39f2ccacf37b2438e78f4886bda06bf92aaeadc33cf7aeb279b5bde804927437"
-    version "0.3.0"
+    url "https://github.com/mocks-rs/mocks/releases/download/0.3.1/mocks-0.3.1-x86_64-apple-darwin.tar.gz"
+    sha256 "04550e51fa913ba4c69ac7abce79b55cc268f5c7f4241716e1bb368f74c2136c"
+    version "0.3.1"
 
     def install
         bin.install "mocks"
